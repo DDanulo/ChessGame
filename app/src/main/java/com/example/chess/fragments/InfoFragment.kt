4f32b282d8b7858903efec14d97fbedf54229d64
danylo <ddv032006@gmai.com>
2023-05-15 14:25:42 +0300

    localisation p1

---
+++ app/src/main/java/com/example/chess/fragments/InfoFragment.kt
@@ -0,0 +1,24 @@
+package com.example.chess.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.chess.R
+import com.example.chess.databinding.FragmentInfoBinding
+import com.example.chess.databinding.FragmentMainBinding
+
+class InfoFragment : Fragment() {
+
+    private lateinit var binding: FragmentInfoBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        binding = FragmentInfoBinding.inflate(inflater, container, false)
+
+
+        return binding.root
+    }
+
+
+}
